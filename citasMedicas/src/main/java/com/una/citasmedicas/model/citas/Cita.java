248ad37d412822d2a1943b698b93a61f7a34ff5a
Eithel2003 <eithel.nunez@gmail.com>
2022-06-24 01:59:08 -0600

    CitaContainer listo
    s

--- citasMedicas/src/main/java/com/una/citasmedicas/model/citas/Cita.java
+++ citasMedicas/src/main/java/com/una/citasmedicas/model/citas/Cita.java
@@ -4,69 +4,41 @@
  */
 package com.una.citasmedicas.model.citas;
 
-import com.una.citasmedicas.AdministracionPaciente.Model.PacienteEntity;
-import com.una.citasmedicas.model.medico.Medico;
+
 
 /**
  *
  * @author villa
  */
 public class Cita {
-    private Integer numCitas;
-    private Medico doctor;
-    private PacienteEntity paciente;
+    private String numCitas;
+    private String idMedico;
+    private String idPaciente;
+    private String hora;   
     private String fecha;
-    private String hora;
 
-    public Cita(Integer numCitas, Medico doctor, PacienteEntity paciente,String fecha, String hora) {
-        this.numCitas = numCitas;
-        this.doctor = doctor;
-        this.paciente = paciente;
-        this.hora = hora;
-        this.fecha = fecha;
+    public Cita(String[] data) {
+        this.numCitas=data[0];
+        this.idMedico=data[1]; 
+        this.idPaciente=data[2];
+        this.hora=data[3];
+        this.fecha=data[4];
     }
 
     /**
      * @return the numCitas
      */
-    public Integer getNumCitas() {
+    public String getNumCitas() {
         return numCitas;
     }
 
     /**
      * @param numCitas the numCitas to set
      */
-    public void setNumCitas(Integer numCitas) {
+    public void setNumCitas(String numCitas) {
         this.numCitas = numCitas;
     }
 
-    /**
-     * @return the doctor
-     */
-    public Medico getDoctor() {
-        return doctor;
-    }
-
-    /**
-     * @param doctor the doctor to set
-     */
-    public void setDoctor(Medico doctor) {
-        this.doctor = doctor;
-    }
-
-    /**
-     * @return the paciente
-     */
-    public PacienteEntity getPaciente() {
-        return paciente;
-    }
-
-    /**
-     * @param paciente the paciente to set
-     */
-    public void setPaciente(PacienteEntity paciente) {
-        this.paciente = paciente;
-    }
 
     /**
      * @return the fecha
@@ -94,6 +66,34 @@ public class Cita {
      */
     public void setHora(String hora) {
         this.hora = hora;
+    }
+
+    /**
+     * @return the idMedico
+     */
+    public String getIdMedico() {
+        return idMedico;
+    }
+
+    /**
+     * @param idMedico the idMedico to set
+     */
+    public void setIdMedico(String idMedico) {
+        this.idMedico = idMedico;
+    }
+
+    /**
+     * @return the idPaciente
+     */
+    public String getIdPaciente() {
+        return idPaciente;
+    }
+
+    /**
+     * @param idPaciente the idPaciente to set
+     */
+    public void setIdPaciente(String idPaciente) {
+        this.idPaciente = idPaciente;
     }
     
     
---
+++ citasMedicas/src/main/java/com/una/citasmedicas/model/citas/controller/CitaController.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.una.citasmedicas.model.citas.controller;
+
+import com.una.citasmedicas.AdministracionPaciente.Model.PacienteContainer;
+import com.una.citasmedicas.model.citas.Cita;
+import com.una.citasmedicas.model.citas.CitaContainer;
+import com.una.citasmedicas.model.medico.MedicoContainer;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Eithel
+ */
+public class CitaController implements CitaInterface{
+    CitaContainer cCita;
+    public CitaController() throws Exception{
+        cCita = new CitaContainer();
+    }
+    
+    @Override
+    public String[][] Citas() throws Exception {
+        ArrayList<Cita> array = cCita.mostrarCita();
+        
+        String[][] matriz = new String[array.size()][5];
+        
+        for (int i = 0; i < array.size(); i++) {
+            Cita auxiliar = array.get(i);
+            
+            matriz[i][0] = auxiliar.getNumCitas();
+            matriz[i][1] = nombreMedico(auxiliar.getIdMedico());
+            matriz[i][2] = nombrePaciente(auxiliar.getIdPaciente());
+            matriz[i][3] = auxiliar.getFecha();
+            matriz[i][4] = auxiliar.getHora();
+        }
+        return matriz;   
+    }
+
+    @Override
+    public String nombrePaciente(String id) throws Exception {
+        PacienteContainer cPaciente = new PacienteContainer();
+        return cPaciente.buscar(id).getNombre();
+    }
+
+    @Override
+    public String nombreMedico(String id)throws Exception {
+        MedicoContainer cMedic = new MedicoContainer();
+        return cMedic.find(id).getNombre();
+
+    }
+    
+}
